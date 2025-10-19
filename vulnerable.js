17d489dadb21200a49a163fdc635efec9aef2010
ChaviruAtthanayake <chaviruatthanayake@gmail.com>
2025-10-19 17:38:12 +0530

    Add vulnerable code for testing

---
+++ vulnerable.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+
+// This will trigger SEC-001: Hardcoded secret
+const SECRET_KEY = "AKIAIOSFODNN7EXAMPLE";
+
+// This will trigger SEC-002: SQL injection
+app.get('/users/:id', (req, res) => {
+  const query = `SELECT * FROM users WHERE id = ${req.params.id}`;
+  db.query(query);
+});
+
+// This will trigger SEC-004: Dangerous eval
+app.post('/calc', (req, res) => {
+  const result = eval(req.body.code);
+  res.json({ result });
+});
+
+app.listen(3000);
